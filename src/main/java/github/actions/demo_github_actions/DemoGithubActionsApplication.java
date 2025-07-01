e6690a97bba2e48d67ad1991edf6d8937cce1e05
denys.biliaiev <>
2025-07-01 10:54:29 +0200

    java source code

---
+++ src/main/java/github/actions/demo_github_actions/DemoGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package github.actions.demo_github_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGithubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/actions/demo_github_actions/DemoGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package github.actions.demo_github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
